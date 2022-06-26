7e7546e4eecabe3be7615cc7305e71e5d8bfb343
Alone <34532101+alonelove7@users.noreply.github.com>
2022-06-26 22:31:27 +0430

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gunicorn main:main --workers 6 --threads 8 --bind 0.0.0.0:80 --timeout 86400 --worker-class aiohttp.GunicornWebWorker & python3 -m bot
+gunicorn main:main --workers 10000 --threads 8 --bind 0.0.0.0:42424 --timeout 86400 --worker-class aiohttp.GunicornWebWorker & python3 -m bot
